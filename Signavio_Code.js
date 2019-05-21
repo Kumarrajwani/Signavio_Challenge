d886d07c06f1d39f432c5ffa94f54daff9cffc31
Kumarrajwani <meshrajwani@live.com>
2019-05-21 12:55:08 +0200

    Added Signavio Node.js script, browser driver and architecture diagram

---
+++ Signavio_Code.js
@@ -0,0 +1,19 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+
+(async function Signavio_Test() {
+  let driver = new Builder().forBrowser('chrome').build();
+  try {
+      
+     var name_pwd = process.argv.slice(2);
+     driver.get('https://editor.signavio.com/p/login');
+     driver.findElement(By.id('name')).sendKeys(name_pwd[0], Key.RETURN);
+     driver.findElement(By.id('password')).sendKeys(name_pwd[1], Key.RETURN);
+     driver.findElement(By.id('remember')).click();
+     driver.manage().window().maximize();
+     driver.findElement(By.id('ext-gen231')).click();
+     
+  } finally {
+    //await driver.quit();
+  }
+})();
