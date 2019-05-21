9b8164bbe28cececa472ec51b421038e149b025b
Kumarrajwani <meshrajwani@live.com>
2019-05-21 18:45:21 +0200

    Final version

--- Signavio_Code.js
+++ Signavio_Code.js
@@ -1,19 +1,20 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
-
-(async function Signavio_Test() {
+(async function Signavio_Challenge() {
   let driver = new Builder().forBrowser('chrome').build();
-  try {
+    try {
       
      var name_pwd = process.argv.slice(2);
      driver.get('https://editor.signavio.com/p/login');
      driver.findElement(By.id('name')).sendKeys(name_pwd[0], Key.RETURN);
      driver.findElement(By.id('password')).sendKeys(name_pwd[1], Key.RETURN);
      driver.findElement(By.id('remember')).click();
-     driver.manage().window().maximize();
-     driver.findElement(By.id('ext-gen231')).click();
+     driver.manage().window().maximize(); 
+     driver.sleep(10000);
+     driver.findElement(By.xpath('//*[starts-with(@id,"ext-gen2")]')).sendKeys('ext-gen2');    
      
-  } finally {
+  } 
+    finally {
     //await driver.quit();
   }
 })();
